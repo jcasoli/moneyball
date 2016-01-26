f0c638890b7b835913c8a5ddc5ec2cfaacf473c6
jordan <casolijordan@gmail.com>
2016-01-26 12:50:43 -0800

    test pull from fantasydata.com

--- src/moneyball.py
+++ src/moneyball.py
@@ -1,9 +1,34 @@
 from flask import Flask
+import httplib, urllib, base64
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print("Hello World!")
+    ########### Python 2.7 #############
+    headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '0deb8f835f264ad99e24cc3622aeb396',
+    }
+
+    params = urllib.urlencode({
+    })
+
+    try:
+        fh = open("temp.txt", 'a')
+        conn = httplib.HTTPSConnection('api.fantasydata.net')
+        conn.request("GET", "/mlb/v2/JSON/teams?key=0deb8f835f264ad99e24cc3622aeb396")
+        print('made it')
+        #conn.request("GET", "/mlb/v2/JSON/teams&%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        return data
+    except Exception as e:
+        print("Fuck")
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 if __name__ == "__main__":
     app.run()
