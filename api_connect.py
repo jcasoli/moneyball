c71f23081fca5816d380c4fab3a27eeaf69adc8c
jordan <casolijordan@gmail.com>
2016-02-02 15:20:52 -0800

    started api_connect

--- api_connect.py
+++ api_connect.py
@@ -1,22 +1,81 @@
 import httplib, urllib, base64
+import sys
 
+active_teams = 'teams'
+are_games_in_progress = 'AreAnyGamesInProgress'
+box_score = 'BoxScore'
+box_scores_by_date = 'BoxScores'
+box_scores_by_date_delta = 'BoxScoresDelta'
 
 class Connection:
     OcpApimSubscriptionKey = '0deb8f835f264ad99e24cc3622aeb396'
     http_loc = 'api.fantasydata.net'
     mlb_path = '/mlb/v2/JSON/'
+    conn = None
 
-
-    def get_connection(self):
-    ########### Python 2.7 #############
+    def __init__(self):
         try:
             conn = httplib.HTTPSConnection('api.fantasydata.net')
-            return conn
         except Exception as e:
-            print("Could not create connection.. here is error: {}".format(e))
+            sys.exit(e)
 
-    def get_data(self, field, date, conn):
-        conn.request("GET", Connection.mlb_path + field + "?key=" + Connection.OcpApimSubscriptionKey)
-        response = conn.getresponse()
-        data = response.read()
-        return data
+
+    def get_active_teams(self):
+        try:
+            self.conn.request("GET", self.mlb_path + active_teams + "?key=" + Connection.OcpApimSubscriptionKey)
+            response = self.conn.getresponse()
+            data = response.read()
+            return data
+        except Exception as e:
+            print "warning: could not get active teams"
+            return []
+
+
+    def get_are_games_in_progress(self):
+        try:
+            self.conn.request("GET", self.mlb_path + are_games_in_progress + "?key=" + Connection.OcpApimSubscriptionKey)
+            response = self.conn.getresponse()
+            data = response.read()
+            return data
+        except Exception as e:
+            print "warning: could not get active teams"
+            return False
+
+
+    def get_box_score(self, gameid):
+        try:
+            self.conn.request("GET", self.mlb_path + box_score + "?key=" + Connection.OcpApimSubscriptionKey)
+            response = self.conn.getresponse()
+            data = response.read()
+            return data
+        except Exception as e:
+            print "warning: could not get active teams"
+            return []
+
+    def get_box_scores_by_date(self, date):
+        try:
+            self.conn.request("GET", self.mlb_path + box_scores_by_date + "/" + date + "?key=" + Connection.OcpApimSubscriptionKey)
+            response = self.conn.getresponse()
+            data = response.read()
+            return data
+        except Exception as e:
+            print "warning: could not get active teams"
+            return []
+
+
+    def get_box_scores_by_date_delta(self, date, delta):
+        try:
+            self.conn.request("GET", self.mlb_path + box_scores_by_date + "/" + date + "/" + delta +
+                            "?key=" + Connection.OcpApimSubscriptionKey)
+            response = self.conn.getresponse()
+            data = response.read()
+            return data
+        except Exception as e:
+            print "warning: could not get active teams"
+            return []
+
+
+
+
+    def close_connection(self):
+        self.conn.close()
