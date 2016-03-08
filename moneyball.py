f73f14b2829689a55882072eec13eff9be21945a
jordan <casolijordan@gmail.com>
2016-03-07 16:48:45 -0800

    change to gitignore

--- moneyball.py
+++ moneyball.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template
+from apscheduler.schedulers.background import BackgroundScheduler
 
-import forms
 import testfactors
 
 import datetime
@@ -9,26 +9,37 @@ import datetime
 app = Flask(__name__)
 app.secret_key = "moneyball"
 
+
+def process():
+    # This method needs to be able to update the current analysis
+    global result
+    # Get instance of test class
+    test = testfactors.Test()
+    # Run analysis for all games on the current date
+    result = test.run(datetime.datetime(2015, 8, 19))
+
+
+
 @app.route("/")
 def home():
+    # Go directly to login page
     return redirect(url_for('login'))
 
-@app.route("/welcome", methods=['GET', 'POST'])
+# route that displays all the games on a given night
+@app.route("/welcome")
 def welcome():
-    global result
-    test = testfactors.Test()
-    result = test.run(datetime.datetime.now())
-    form = forms.DateForm()
-    if form.validate_on_submit():
-        date = form.dt.data
-        result = test.run(date)
-        return render_template('welcome.html', result=result, form=form)
-    return render_template('welcome.html', result=result, form=form)
+    # Render webpage with result (the latest version of result)
+    return render_template('welcome.html', result=result)
 
+
+# route for dislplaying the results of an analysis
 @app.route("/results")
 def results():
+    # Get gameid from url parameter 'gameid'
     gameid = request.args.get('gameid').encode('ascii', 'ignore')
+    # Get dictionary corresponding to the game that was clicked
     game = [match for match in result if str(match['GameID'])==gameid]
+    # Display the webpage
     return render_template('results.html', game=game[0])
 
 
@@ -46,4 +57,11 @@ def login():
 
 if __name__ == "__main__":
     global result
-    app.run(debug=True)
+    result = []
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(process, 'interval', minutes=1)
+    scheduler.start()
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        scheduler.shutdown()
--- test_functions.py
+++ test_functions.py
@@ -10,36 +10,36 @@ import json
 # conn = apiconnect.Connection()
 # key = '0deb8f835f264ad99e24cc3622aeb396'
 
-def test_win_streak(homeTeam, awayTeam, conn, date):
-    streak1 = 0
-    streak2 = 0
-    currentDate = date
-    dateString = '%s-%s-%s' % (date.year, date.month, date.day)
-    if (date.month<10 and date.day<10):
-         dateString = '%s-0%s-0%s' % (date.year, date.month, date.day)
-    if (date.month<10 and date.day>9):
-         dateString = '%s-0%s-%s' % (date.year, date.month, date.day)
-    if (date.month>9 and date.day<10):
-         dateString = '%s-%s-%s' % (date.year, date.month, date.day)
-
-#       Check Away Team Streak
-    x = didWin(homeTeam, conn, dateString)
-    while (x<2):
-        if x == 1:
-            streak1 = streak1 + 1
-        currentDate = currentDate - timedelta(days=1.0)
-        dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
-        x = didWin(homeTeam, conn, dateString)
-    currentDate = date
-    dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
-    x = didWin(awayTeam, conn, dateString)
-    while (x<2):
-        if x == 1:
-            streak2 = streak2 + 1
-        currentDate = currentDate - timedelta(days=1.0)
-        dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
-        x = didWin(awayTeam, conn, dateString)
-    return "%s win streak is at %s, %s win streak is at %s" % (homeTeam, streak1, awayTeam, streak2)
+# def test_win_streak(homeTeam, awayTeam, conn, date):
+#     streak1 = 0
+#     streak2 = 0
+#     currentDate = date
+#     dateString = '%s-%s-%s' % (date.year, date.month, date.day)
+#     if (date.month<10 and date.day<10):
+#          dateString = '%s-0%s-0%s' % (date.year, date.month, date.day)
+#     if (date.month<10 and date.day>9):
+#          dateString = '%s-0%s-%s' % (date.year, date.month, date.day)
+#     if (date.month>9 and date.day<10):
+#          dateString = '%s-%s-%s' % (date.year, date.month, date.day)
+#
+# #       Check Away Team Streak
+#     x = didWin(homeTeam, conn, dateString)
+#     while (x<2):
+#         if x == 1:
+#             streak1 = streak1 + 1
+#         currentDate = currentDate - timedelta(days=1.0)
+#         dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
+#         x = didWin(homeTeam, conn, dateString)
+#     currentDate = date
+#     dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
+#     x = didWin(awayTeam, conn, dateString)
+#     while (x<2):
+#         if x == 1:
+#             streak2 = streak2 + 1
+#         currentDate = currentDate - timedelta(days=1.0)
+#         dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
+#         x = didWin(awayTeam, conn, dateString)
+#     return "%s win streak is at %s, %s win streak is at %s" % (homeTeam, streak1, awayTeam, streak2), 5
 
 def test_pitching_today(homeTeam, awayTeam, conn, date,):
     homePitcher = ''
@@ -52,7 +52,7 @@ def test_pitching_today(homeTeam, awayTeam, conn, date,):
             homePitcher = dict['HomeTeamProbablePitcherID']
         if dict['AwayTeam'] == awayTeam:
             awayPitcher = dict['AwayTeamProbablePitcherID']
-    return '%s is starting for %s against %s for %s' %(homePitcher, homeTeam, awayPitcher, awayTeam)
+    return '%s is starting for %s against %s for %s' %(homePitcher, homeTeam, awayPitcher, awayTeam), -3
 
 def test_trip_length(homeTeam, awayTeam, conn, date):
     currentDate = date
@@ -69,7 +69,7 @@ def test_trip_length(homeTeam, awayTeam, conn, date):
         currentDate = currentDate - timedelta(days=1.0)
         dateString = '%s-%s-%s' % (currentDate.year, currentDate.month, currentDate.day)
         x = isAway(conn, dateString, awayTeam)
-    return '%s on %s game of road trip' %(awayTeam, length)
+    return '%s on %s game of road trip' %(awayTeam, length), 7
 
 def didWin(team_id1, conn, date):
     data = conn.get_games_by_date(date)
@@ -100,8 +100,8 @@ def isAway(conn, date, team):
     return holder
 
 if __name__ == "__main__" :
-
-    test_pitching_today("HOU", "TOR", conn, today)
-    test_trip_length("HOU", "TOR", conn, today)
-    test_win_streak("HOU", "TOR", conn, today)
+    pass
+    # test_pitching_today("HOU", "TOR", conn, today)
+    # test_trip_length("HOU", "TOR", conn, today)
+    # test_win_streak("HOU", "TOR", conn, today)
 
